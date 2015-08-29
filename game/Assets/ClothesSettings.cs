d672767b150fd43b7d9075dbb1269f14e03f940d
pontura <pontura@gmail.com>
2015-08-29 19:04:08 -0300

    clothes

--- game/Assets/ClothesSettings.cs
+++ game/Assets/ClothesSettings.cs
@@ -6,44 +6,100 @@ using System;
 
 public class ClothesSettings : MonoBehaviour {
 
-    public Images[] types;
+    public Parts[] parts;
 
     [Serializable]
-    public class Images
+    public class Parts
     {
-        public List<string> glasses;
-        public List<string> shoes;
-        public List<string> tops;
-        public List<string> hairs;
-        public List<string> legs;
-        public List<string> faces;
+        public List<ClothesPart> Clothes;
+        public List<FacesPart> Faces;
+        public List<HairsPart> Hairs;
+    }
+    [Serializable]
+    public class ClothesPart
+    {
+        public string part;
+        public string sex;
+        public string style;
+        public string subPart;
+    }
+
+    [Serializable]
+    public class FacesPart
+    {
+        public string type;
+        public string sex;
+        public string expresion;
+    }
+
+    [Serializable]
+    public class HairsPart
+    {
+        public string type;
+        public string sex;
     }
 
 	void Start () {
-        LoadArray(types[0].glasses, @"Assets\Resources\Victims\A\Body");
-        //LoadArray(shoes, @"images\shoes\");
-        //LoadArray(tops, @"images\top\");
-        //LoadArray(hairs, @"images\hair\");
-        //LoadArray(legs, @"images\bottom\");
-        //LoadArray(faces, @"images\face\");
-	}
+        List<string> clothes = LoadArray(@"Assets\Resources\Victims\A\Clothes");
+        List<string> faces = LoadArray(@"Assets\Resources\Victims\A\Face");
+        List<string> hairs = LoadArray(@"Assets\Resources\Victims\A\Hair");
 
-    private void LoadArray(List<string> arr, string path)
+        GetClothes(clothes, parts[0].Clothes);
+        GetFaces(faces, parts[0].Faces);
+        GetHairs(hairs, parts[0].Hairs);
+	}
+    private void GetClothes(List<string> arr, List<ClothesPart> Arr)
     {
-        string lastName = "aaaa";
+        foreach (string path in arr)
+        {
+            String[] textSplit = path.Split("_"[0]);
+
+            ClothesPart part = new ClothesPart();
+            part.part = textSplit[0];
+            part.sex = textSplit[1];
+            part.style = textSplit[3];
+            part.subPart = textSplit[4];
+
+            Arr.Add(part);
+        }
+    }
+    private void GetFaces(List<string> arr, List<FacesPart> Arr)
+    {
+        foreach (string path in arr)
+        {
+            String[] textSplit = path.Split("_"[0]);
+
+            FacesPart part = new FacesPart();
+            part.type = textSplit[1];
+            part.sex = textSplit[0];
+            part.expresion = textSplit[2];
+
+            Arr.Add(part);
+        }
+    }
+    private void GetHairs(List<string> arr, List<HairsPart> Arr)
+    {
+        foreach (string path in arr)
+        {
+            String[] textSplit = path.Split("_"[0]);
+
+            HairsPart part = new HairsPart();
+            part.type = textSplit[3];
+            part.sex = textSplit[1];
+
+            Arr.Add(part);
+        }
+    }
+    private List<string> LoadArray(string path)
+    {
+        List<string> newArr = new List<string>();
 
         foreach (string name in System.IO.Directory.GetFiles(path, "*.png"))
         {
-            String[] textSplit = name.Split("/"[0])[0].Split("_"[0]);
-
-
-            string realName = textSplit[0] + "_" + textSplit[1];
-
-            if (realName != lastName)
-            {
-                arr.Add(realName);
-                lastName = realName;
-            }
+            String[] textSplit = name.Split(@"\"[0]);
+            String[] textSplit2 = textSplit[5].Split("."[0]);
+            newArr.Add(textSplit2[0]);
         }
+        return newArr;
     }
 }
